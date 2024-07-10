290e2557e7aa7619d47ddff3afea9224c02d553b
Edwin <edwin.rybarczyk@liberis.com>
2024-07-10 09:26:03 +0100

    c

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,23 +1,24 @@
 local wezterm = require 'wezterm'
 local config = {}
 
--- config.color_scheme = "Catppuccin Mocha"
--- config.color_scheme = "Catppuccin Macchiato"
+-- function scheme_for_appearance(appearance)
+--     if appearance:find "Dark" then
+--         return "Catppuccin Mocha"
+--     else
+--         return "Catppuccin Latte"
+--     end
+-- end
 
-function scheme_for_appearance(appearance)
-    if appearance:find "Dark" then
-        return "Catppuccin Mocha"
-    else
-        return "Catppuccin Latte"
-    end
-end
-
-config.color_scheme = scheme_for_appearance(wezterm.gui.get_appearance())
+-- config.color_scheme = scheme_for_appearance(wezterm.gui.get_appearance())
+config.color_scheme = "Catppuccin Mocha"
 
 config.use_fancy_tab_bar = false
 config.hide_tab_bar_if_only_one_tab = true
+
+config.adjust_window_size_when_changing_font_size = false
+
 -- config.freetype_load_flags = 'NO_HINTING'
-config.font_antialias = 'Subpixel'
+-- config.font_antialias = 'Subpixel'
 -- config.font_antialias = 'Greyscale'
 
 config.font = wezterm.font_with_fallback {
@@ -27,7 +28,7 @@ config.font = wezterm.font_with_fallback {
 }
 -- config.front_end = "WebGpu"
 -- config.freetype_load_target = "HorizontalLcd"
-config.font_size = 12.0
+config.font_size = 12.5
 config.initial_cols = 100
 config.initial_rows = 25
 
