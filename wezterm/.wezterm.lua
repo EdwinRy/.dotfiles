e0418d8e6271912b9680ec842fb0233bdb935d27
Edwin <edwin.rybarczyk@liberis.com>
2024-08-22 17:12:18 +0100

    changes

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -48,169 +48,145 @@ config.leader = { key = 'a', mods = 'CMD' }
 local resizeFactor = 15
 local bigResizeFactor = 50
 
-config.keys = {
-    {
-        key = 'v',
-        mods = 'LEADER',
-        action = wezterm.action {
-            SplitHorizontal = { domain = "CurrentPaneDomain" }
-        }
-    },
-    {
-        key = '%',
-        mods = 'LEADER',
-        action = wezterm.action {
-            SplitVertical = { domain = "CurrentPaneDomain" }
-        }
-    },
-    {
-        key = 'h',
-        mods = 'LEADER',
-        action = wezterm.action {
-            ActivatePaneDirection = "Left"
-        }
-    },
-    {
-        key = 'l',
-        mods = 'LEADER',
-        action = wezterm.action {
-            ActivatePaneDirection = "Right"
-        }
-    },
-    {
-        key = 'j',
-        mods = 'LEADER',
-        action = wezterm.action {
-            ActivatePaneDirection = "Down"
-        }
-    },
-    {
-        key = 'k',
-        mods = 'LEADER',
-        action = wezterm.action {
-            ActivatePaneDirection = "Up"
-        }
-    },
-    {
-        key = 'h',
-        mods = 'CMD',
-        action = wezterm.action {
-            ActivatePaneDirection = "Left"
-        }
-    },
-    {
-        key = 'l',
-        mods = 'CMD',
-        action = wezterm.action {
-            ActivatePaneDirection = "Right"
-        }
-    },
-    {
-        key = 'j',
-        mods = 'CMD',
-        action = wezterm.action {
-            ActivatePaneDirection = "Down"
-        }
-    },
-    {
-        key = 'k',
-        mods = 'CMD',
-        action = wezterm.action {
-            ActivatePaneDirection = "Up"
-        }
-    },
-    {
-        key = '[',
-        mods = "CMD",
-        action = wezterm.action {
-            ActivateTabRelative = -1
-        }
-    },
-    {
-        key = ']',
-        mods = "CMD",
-        action = wezterm.action {
-            ActivateTabRelative = 1
-        }
-    },
-    {
-        key = '[',
-        mods = "CMD|META",
-        action = wezterm.action {
-            MoveTabRelative = -1
-        }
-    },
-    {
-        key = ']',
-        mods = "CMD|META",
-        action = wezterm.action {
-            MoveTabRelative = 1
-        }
-    },
-    {
-        key = 't',
-        mods = 'CMD',
-        action = wezterm.action {
-            SpawnTab = "DefaultDomain"
-        }
-    },
-    {
-        key = 'h',
-        mods = 'CMD|META',
-        action = wezterm.action {
-            AdjustPaneSize = { 'Left', resizeFactor }
-        }
-    },
-    {
-        key = 'l',
-        mods = 'CMD|META',
-        action = wezterm.action {
-            AdjustPaneSize = { 'Right', resizeFactor }
-        }
-    },
-    {
-        key = 'k',
-        mods = 'CMD|META',
-        action = wezterm.action {
-            AdjustPaneSize = { 'Up', resizeFactor }
-        }
-    },
-    {
-        key = 'j',
-        mods = 'CMD|META',
-        action = wezterm.action {
-            AdjustPaneSize = { 'Down', resizeFactor }
-        }
-    },
+local function splitKeys()
+    local keys = {
+        {
+            key = 'v',
+            mods = 'LEADER',
+            action = wezterm.action {
+                SplitHorizontal = { domain = "CurrentPaneDomain" }
+            }
+        },
+        {
+            key = '%',
+            mods = 'LEADER',
+            action = wezterm.action {
+                SplitVertical = { domain = "CurrentPaneDomain" }
+            }
+        },
+    }
+    return keys
+end
 
-    {
-        key = 'h',
-        mods = 'CMD|META|SHIFT',
-        action = wezterm.action {
-            AdjustPaneSize = { 'Left', bigResizeFactor }
-        }
-    },
-    {
-        key = 'l',
-        mods = 'CMD|META|SHIFT',
-        action = wezterm.action {
-            AdjustPaneSize = { 'Right', bigResizeFactor }
-        }
-    },
-    {
-        key = 'k',
-        mods = 'CMD|META|SHIFT',
-        action = wezterm.action {
-            AdjustPaneSize = { 'Up', bigResizeFactor }
-        }
-    },
-    {
-        key = 'j',
-        mods = 'CMD|META|SHIFT',
-        action = wezterm.action {
-            AdjustPaneSize = { 'Down', bigResizeFactor }
-        }
-    },
+local function paneNavigationKeys()
+    local keyConfig = {
+        { key = 'h', direction = 'Left', mod = 'LEADER' },
+        { key = 'j', direction = 'Down', mod = 'LEADER'},
+        { key = 'k', direction = 'Up', mod = 'LEADER'},
+        { key = 'l', direction = 'Right', mod = 'LEADER'},
+
+        { key = 'h', direction = 'Left', mod = 'CMD' },
+        { key = 'j', direction = 'Down', mod = 'CMD'},
+        { key = 'k', direction = 'Up', mod = 'CMD'},
+        { key = 'l', direction = 'Right', mod = 'CMD'},
+
+        { key = 'LeftArrow', direction = 'Left', mod = 'LEADER' },
+        { key = 'DownArrow', direction = 'Down', mod = 'LEADER'},
+        { key = 'UpArrow', direction = 'Up', mod = 'LEADER'},
+        { key = 'RightArrow', direction = 'Right', mod = 'LEADER'},
+
+        { key = 'LeftArrow', direction = 'Left', mod = 'CMD' },
+        { key = 'DownArrow', direction = 'Down', mod = 'CMD'},
+        { key = 'UpArrow', direction = 'Up', mod = 'CMD'},
+        { key = 'RightArrow', direction = 'Right', mod = 'CMD'},
+    }
+
+    local keys = {}
+
+    for _, c in pairs(keyConfig) do
+        table.insert(keys, {
+            key = c.key,
+            mods = c.mod,
+            action = wezterm.action {
+                ActivatePaneDirection = c.direction
+            }
+        })
+    end
+
+    return keys
+end
+
+local function tabNavigationKeys()
+    return {
+        {
+            key = '[',
+            mods = "CMD",
+            action = wezterm.action {
+                ActivateTabRelative = -1
+            }
+        },
+        {
+            key = ']',
+            mods = "CMD",
+            action = wezterm.action {
+                ActivateTabRelative = 1
+            }
+        },
+        {
+            key = '[',
+            mods = "CMD|META",
+            action = wezterm.action {
+                MoveTabRelative = -1
+            }
+        },
+        {
+            key = ']',
+            mods = "CMD|META",
+            action = wezterm.action {
+                MoveTabRelative = 1
+            }
+        },
+        {
+            key = 't',
+            mods = 'CMD',
+            action = wezterm.action {
+                SpawnTab = "DefaultDomain"
+            }
+        },
+    }
+end
+
+
+local function paneResizeKeys()
+    local keyConfig = {
+        { key = 'h', direction = 'Left', mod = 'CMD|META', resizeFactor = resizeFactor },
+        { key = 'j', direction = 'Down', mod = 'CMD|META', resizeFactor = resizeFactor },
+        { key = 'k', direction = 'Up', mod = 'CMD|META', resizeFactor = resizeFactor },
+        { key = 'l', direction = 'Right', mod = 'CMD|META', resizeFactor = resizeFactor },
+
+        { key = 'h', direction = 'Left', mod = 'CMD|META|SHIFT', resizeFactor = bigResizeFactor },
+        { key = 'j', direction = 'Down', mod = 'CMD|META|SHIFT', resizeFactor = bigResizeFactor },
+        { key = 'k', direction = 'Up', mod = 'CMD|META|SHIFT', resizeFactor = bigResizeFactor },
+        { key = 'l', direction = 'Right', mod = 'CMD|META|SHIFT', resizeFactor = bigResizeFactor },
+
+        { key = 'LeftArrow', direction = 'Left', mod = 'CMD|META', resizeFactor = resizeFactor },
+        { key = 'DownArrow', direction = 'Down', mod = 'CMD|META', resizeFactor = resizeFactor },
+        { key = 'UpArrow', direction = 'Up', mod = 'CMD|META', resizeFactor = resizeFactor },
+        { key = 'RightArrow', direction = 'Right', mod = 'CMD|META', resizeFactor = resizeFactor },
+
+        { key = 'LeftArrow', direction = 'Left', mod = 'CMD|META|SHIFT', resizeFactor = bigResizeFactor },
+        { key = 'DownArrow', direction = 'Down', mod = 'CMD|META|SHIFT', resizeFactor = bigResizeFactor },
+        { key = 'UpArrow', direction = 'Up', mod = 'CMD|META|SHIFT', resizeFactor = bigResizeFactor },
+        { key = 'RightArrow', direction = 'Right', mod = 'CMD|META|SHIFT', resizeFactor = bigResizeFactor },
+    }
+    
+    local keys = {}
+
+    for _, c in pairs(keyConfig) do
+        table.insert(keys, {
+            key = c.key,
+            mods = c.mod,
+            action = wezterm.action {
+                AdjustPaneSize = { c.direction, c.resizeFactor }
+            }
+        })
+    end
+
+    return keys
+end
+
+
+config.keys = {
     {
         key = 'x',
         mods = 'LEADER',
@@ -245,6 +221,18 @@ config.keys = {
     },
 }
 
+local extraKeys = {
+    splitKeys(),
+    paneNavigationKeys(),
+    tabNavigationKeys(),
+    paneResizeKeys(),
+}
+
+for _, keys in ipairs(extraKeys) do
+    for _, k in ipairs(keys) do
+        table.insert(config.keys, k)
+    end
+end
 
 wezterm.on('format-window-title', function(tab, pane, tabs, panes, config)
     local zoomed = ''
